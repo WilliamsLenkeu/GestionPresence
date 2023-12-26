a85ce8a48da183ac87f9a8337895cdcc0ecff87c
WallyDev <williams25prince@gmail.com>
2023-12-26 21:20:46 +0100

    update

--- index.php
+++ index.php
@@ -1,18 +1,108 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>GestPresence</title>
+    <title>Tableau de Bord - Administrateur</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css" type="text/css" />
   </head>
-  <body>
-    <div class="container-fluid">
-        <!-- Section de la barre de navigation -->
-        <div class="row">
-            <?php
-                include './navbar.php';
-            ?>
+  <body class="text-bg-dar">
+    <div class="container-fluid dash">
+        
+        <!-- Section du tableau de bord -->
+        <div class="row dash-1">
+            <div class="col-2 border-end border-secondary text-center">
+                <?php
+                    include './navbar.php';
+                ?>    
+            </div>
+            <div class="col mx-3">
+                <div class="row">
+                    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                        <div class="container-fluid">
+                        <a class="navbar-brand" href="#">Navbar</a>
+                        </div>
+                    </nav>
+                </div>
+                <div class="row mt-4">
+                    <!-- Carte des étudiants avec le plus d'absences -->
+                    <div class="col-md-6 my-2">
+                        <div class="card">
+                            <div class="card-header">
+                                Étudiants avec le plus d'absences
+                            </div>
+                            <div class="card-body">
+                                <!-- Afficher la liste des étudiants avec le plus d'absences ici -->
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Carte des notifications -->
+                    <div class="col-md-6 my-2">
+                        <div class="card">
+                            <div class="card-header">
+                                Notifications
+                            </div>
+                            <div class="card-body">
+                                <!-- Afficher la liste des notifications ici -->
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 my-2">
+                        <div class="card">
+                            <div class="card-header">
+                                Notifications
+                            </div>
+                            <div class="card-body">
+                                <!-- Afficher la liste des notifications ici -->
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 my-2">
+                        <div class="card">
+                            <div class="card-header">
+                                Notifications
+                            </div>
+                            <div class="card-body">
+                                <!-- Afficher la liste des notifications ici -->
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 my-2">
+                        <div class="card">
+                            <div class="card-header">
+                                Notifications
+                            </div>
+                            <div class="card-body">
+                                <!-- Afficher la liste des notifications ici -->
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 my-2">
+                        <div class="card">
+                            <div class="card-header">
+                                Notifications
+                            </div>
+                            <div class="card-body">
+                                <!-- Afficher la liste des notifications ici -->
+                            </div>
+                        </div>
+                    </div>
+
+                    
+
+                    <!-- Ajoutez d'autres cartes pour d'autres fonctionnalités -->
+
+                </div>
+            </div>
+            <div class="col-2 bg-danger">
+                
+            </div>
         </div>
     </div>
 
--- navbar.php
+++ navbar.php
@@ -1,44 +1,56 @@
-<nav class="navbar navbar-expand-lg text-bg-dark navbar-dark">
+<nav class="navbar navbar-expand-lg flex-column border-bottom border-secondary h-100">
     <div class="container-fluid">
-        <a class="navbar-brand m-auto" href="#">GestPresence</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav pt-1 me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Dashboard</a>
-                </li>
-                <li class="nav-item"><a class="nav-link" href="#">Liste d'Appels</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Etudiants</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Cours</a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Parametres
+            <ul class="navbar-nav pt-1 flex-column me-auto mb-2 mb-lg-0 w-100">
+                <liv class="nav-item mt-2 mb-3">
+                    <a class="navbar-brand" href="./index.php">GestPresence</a>
+                </liv>
+                <li class="nav-item btn btn-outline-secondary my-2">
+                    <a class="nav-link" aria-current="page" href="./index.php">
+                      <svg class="mb-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-speedometer2" viewBox="0 0 16 16">
+                            <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4M3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10m9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5m.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z"/>
+                            <path fill-rule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10m8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3"/>
+                        </svg>
+                        Dashboard
                     </a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a class="dropdown-item m-auto" href="#">
-                                <svg class="m-auto" xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-person-vcard-fill" viewBox="0 0 16 16">
-                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm9 1.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4a.5.5 0 0 0-.5.5M9 8a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4A.5.5 0 0 0 9 8m1 2.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 0-1h-3a.5.5 0 0 0-.5.5m-1 2C9 10.567 7.21 9 5 9c-2.086 0-3.8 1.398-3.984 3.181A1 1 0 0 0 2 13h6.96c.026-.163.04-.33.04-.5M7 6a2 2 0 1 0-4 0 2 2 0 0 0 4 0"/>
-                                </svg>
-                                <b class="m-auto">Etudiant</b>
-                            </a>
-                        </li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
+                </li>
+                <li class="nav-item btn btn-outline-secondary my-2">
+                    <a class="nav-link" href="#">
+                        <svg class="mb-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
+                            <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2z"/>
+                            <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0M7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0"/>
+                        </svg>
+                        Liste d'Appels
+                    </a>
+                </li>
+                <li class="nav-item btn btn-outline-secondary my-2">
+                    <a class="nav-link" href="#">
+                        <svg class="mb-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-badge-fill" viewBox="0 0 16 16">
+                            <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6m5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1z"/>
+                        </svg>
+                        Etudiants
+                    </a>
+                </li>
+                <li class="nav-item btn btn-outline-secondary my-2">
+                    <a class="nav-link" href="#">
+                        <svg class="mb-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-book-half" viewBox="0 0 16 16">
+                            <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492V2.687zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+                        </svg>
+                        Cours
+                    </a>
                 </li>
             </ul>
-            <form class="d-flex" role="search">
+            <!-- <form class="d-flex" role="search">
                 <input class="form-control" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-dark" type="submit">
                     <svg class="m-auto" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
                     </svg>
                 </button>
-            </form>
+            </form> -->
         </div>
     </div>
 </nav>
