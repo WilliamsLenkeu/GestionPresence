c5585606eb7007b09e0bba16e8405c23e0fb8813
WallyDev <williams25prince@gmail.com>
2023-12-29 13:01:37 +0100

    update

--- templates/user-card.php
+++ templates/user-card.php
@@ -1,5 +1,4 @@
 <?php
-
 // Vérifiez si l'utilisateur est connecté en vérifiant la présence de son matricule dans la session
 if (isset($_SESSION['matricule'])) {
     // Récupérez le matricule de la session
@@ -30,7 +29,7 @@ if (isset($_SESSION['matricule'])) {
         $stmt->bind_result($storedMatricule, $storedUsername, $nom, $prenom, $dateNaissance, $role);
         $stmt->fetch();
 ?>
-        <div class="card my-3 shadow-sm">
+        <div class="card my-3 shadow-sm" style="background-color: c4c4c4;">
             <div class="card-header">
                 <h5 class="card-title mb-0">Informations Utilisateur</h5>
             </div>
@@ -45,8 +44,23 @@ if (isset($_SESSION['matricule'])) {
                 <!-- Ajoutez d'autres informations de l'utilisateur ici -->
             </div>
             <div class="card-footer bg-transparent border-top-0">
-                <!-- Bouton pour accéder au profil -->
-                <a href="#" class="btn btn-light btn-sm">Voir Profil</a>
+                <div class="container ">
+                    <div class="row">
+                        <div class="col-6">
+                            <!-- Bouton pour accéder au profil -->
+                            <a href="#" class="btn btn-light btn-sm w-100 shadow-sm border-1">Profil</a>
+                        </div>
+                        <div class="col-6">
+                            <!-- Bouton de déconnexion -->
+                            <a href="../logout.php" class="btn btn-danger btn-sm w-100 shadow- border-danger">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+                                    <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 <?php
