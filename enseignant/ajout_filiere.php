c158f71b34baabd9dddeee117a7c668cdc3c39d3
emmanuelle340 <emmanuellekenmogne340@gmail.com>
2023-12-31 10:29:35 +0100

    update

---
+++ enseignant/ajout_filiere.php
@@ -0,0 +1,105 @@
+<?php
+// Démarrez la session pour accéder aux variables de session
+session_start();
+
+include '../connexion.php';
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("La connexion à la base de données avec MySQLi a échoué : " . $conn->connect_error);
+}
+
+// Utilisation d'une requête préparée pour éviter les attaques par injection SQL
+$sql = "SELECT administrateur FROM utilisateur WHERE matricule = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('s', $_SESSION['matricule']);
+$stmt->execute();
+$stmt->store_result();
+
+// Vérifier si l'utilisateur est administrateur
+$isAdmin = false;
+if ($stmt->num_rows > 0) {
+    $stmt->bind_result($adminStatus);
+    $stmt->fetch();
+    $isAdmin = $adminStatus == 1;
+}
+
+// Vérifier si l'utilisateur est administrateur, sinon rediriger
+if (!$isAdmin) {
+    header('Location: index.php'); // Remplacez "index.php" par la page de redirection appropriée
+    exit;
+}
+
+// Traitement du formulaire d'ajout de filière
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST['nom'];
+    $description = $_POST['description'];
+
+    // Utilisation d'une requête préparée pour éviter les attaques par injection SQL
+    $sqlAjoutFiliere = "INSERT INTO filiere (nom, description) VALUES (?, ?)";
+    $stmtAjoutFiliere = $conn->prepare($sqlAjoutFiliere);
+    $stmtAjoutFiliere->bind_param('ss', $nom, $description);
+
+    if ($stmtAjoutFiliere->execute()) {
+        header('Location: liste_filieres.php'); // Rediriger après l'ajout de la filière
+        exit;
+    } else {
+        echo 'Une erreur est survenue lors de l\'ajout de la filière. Veuillez réessayer.';
+    }
+
+    // Fermer la déclaration
+    $stmtAjoutFiliere->close();
+}
+
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ajouter une Filière</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/style.css" type="text/css" />
+    <link rel="icon" href="../image/logo.jpg" type="image/x-icon">
+</head>
+
+<body class="fw-bold">
+    <div class="container-fluid dash">
+
+        <!-- Section du tableau de bord -->
+        <div class="row dash-1">
+            <div class="col-md-2 border-end border-secondary text-center">
+                <?php include './navbar.php'; ?>
+            </div>
+            <div class="col-md col-12">
+                <div class="row page-title shadow-lg">
+                    <div class="fs-2 mt-3"> Ajouter une Filière </div>
+                </div>
+                <div class="row mt-4 fw-normal">
+                    <!-- Formulaire d'ajout de filière -->
+                    <div class="col-md-6">
+                        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                            <div class="mb-3">
+                                <label for="nom" class="form-label">Nom de la Filière :</label>
+                                <input type="text" class="form-control" name="nom" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="description" class="form-label">Description :</label>
+                                <textarea class="form-control" name="description" rows="3" required></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Ajouter</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- Colonne de droite pour les informations de l'utilisateur -->
+            <div class="col-md-2 border-start border-secondary">
+                <?php include '../templates/user-card.php'; ?>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap Script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
--- etudiant/dashboard_etudiant.php
+++ etudiant/dashboard_etudiant.php
@@ -21,10 +21,18 @@ $stmt->execute();
 $stmt->store_result();
 
 if ($stmt->num_rows == 0) {
-    // L'enseignant n'a pas de profil, rediriger vers la page de profil
-    header('Location: remplir_profil_enseignant.php');
+    // L'etudiant n'a pas de profil, rediriger vers la page de profil
+    header('Location: remplir_profil_etudiant.php');
     exit;
 }
+
+// Récupérer la liste des étudiants avec leur matricule et la somme de leurs heures d'absence
+$sqlListeEtudiants = "SELECT u.matricule, p.nom, p.prenom, COALESCE(SUM(ea.heures_attribuees), 0) AS total_heures_absence
+                     FROM utilisateur u
+                     JOIN profil p ON u.matricule = p.utilisateur_matricule
+                     LEFT JOIN enregistrement_assiduite ea ON u.matricule = ea.utilisateur_matricule
+                     GROUP BY u.matricule, p.nom, p.prenom";
+$resultListeEtudiants = $conn->query($sqlListeEtudiants);
 
 ?>
 
@@ -34,7 +42,7 @@ if ($stmt->num_rows == 0) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Tableau de Bord - Administrateur</title>
+    <title>Tableau de Bord - Enseignant</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css" type="text/css" />
     <link rel="stylesheet" href="../css/style.css" type="text/css" />
@@ -45,7 +53,46 @@ if ($stmt->num_rows == 0) {
     <div class="container-fluid dash">
 
         <!-- Section du tableau de bord -->
+        <div class="row dash-1">
+            <div class="col-md-2 border-end border-secondary text-center">
+                <?php
+                include './navbar.php';
+                ?>
+            </div>
+            <div class="col-md col-12">
+                <div class="row page-title shadow-lg">
+                    <div class="fs-2 mt-3"> Tableau de Bord </div>
                 </div>
+                <div class="row mt-4 fw-normal">
+                    <!-- Liste des étudiants avec la somme de leurs heures d'absence -->
+                    <?php
+                    if ($resultListeEtudiants->num_rows > 0) {
+                        while ($rowEtudiant = $resultListeEtudiants->fetch_assoc()) {
+                            echo '<div class="col-md-4 mb-4">';
+                            echo '<div class="card">';
+                            echo '<div class="card-body">';
+                            echo '<h5 class="card-title">' . $rowEtudiant['nom'] . ' ' . $rowEtudiant['prenom'] . '</h5>';
+                            echo '<p class="card-text">Matricule : ' . $rowEtudiant['matricule'] . '</p>';
+                            echo '<p class="card-text">Total heures d\'absence : ' . $rowEtudiant['total_heures_absence'] . '</p>';
+                            echo '</div>';
+                            echo '</div>';
+                            echo '</div>';
+                        }
+                    } else {
+                        echo '<div class="col-md-12">';
+                        echo '<p>Aucun étudiant trouvé.</p>';
+                        echo '</div>';
+                    }
+                    ?>
+                </div>
+            </div>
+            <!-- Colonne de droite pour les informations de l'utilisateur -->
+            <div class="col-md-2 border-start border-secondary">
+                <?php
+                include '../templates/user-card.php';
+                ?>
+            </div>
+        </div>
     </div>
 
     <!-- Bootstrap Script -->
